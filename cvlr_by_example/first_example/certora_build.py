1aa4b02c1917ef0aa03866f9db687a07e61e80f6
phreppo <parolini.francesco.1@gmail.com>
2025-02-17 12:19:06 +0000

    RUSTFLAGS are passed in the environment and not as strings when calling the command

--- cvlr_by_example/first_example/certora_build.py
+++ cvlr_by_example/first_example/certora_build.py
@@ -12,7 +12,7 @@ SCRIPT_DIR = Path(__file__).resolve().parent
 # This is the recommended list of flags to use to compile Solana projects.
 RUSTFLAGS = "-C llvm-args=--sbf-expand-memcpy-in-order -C llvm-args=--combiner-store-merging=false -C llvm-args=--combiner-load-merging=false -C llvm-args=--aggressive-instcombine-max-scan-instrs=0 -C llvm-args=--combiner-reduce-load-op-store-width=false -C llvm-args=--combiner-shrink-load-replace-store-with-store=false -C strip=none -C debuginfo=2"
 # Command to run for compiling the rust project.
-COMMAND = "RUSTFLAGS='{}' cargo +solana build-sbf".format(RUSTFLAGS)
+COMMAND = "cargo +solana build-sbf".format(RUSTFLAGS)
 
 # JSON FIELDS
 PROJECT_DIR = (SCRIPT_DIR).resolve()
