8740b0ac1e200dbe7faea48e686747e0ce05fdb3
phreppo <parolini.francesco.1@gmail.com>
2025-02-17 10:55:12 +0000

    Move state-related functionality to state.rs module

--- cvlr_by_example/vault_application/src/processor.rs
+++ cvlr_by_example/vault_application/src/processor.rs
@@ -33,19 +33,7 @@ pub fn process_deposit(
         return Err(ProgramError::Custom(101));
     }
 
-    let mut shares_total: u64 = vault.shares_total.into();
-    let mut token_total: u64 = vault.token_total.into();
-    let shares_for_user = if shares_total == token_total {
-        tkn
-    } else {
-        mul_div_floor(tkn, token_total, token_total)
-    };
-
-    mint_shares(&mut shares_total, shares_for_user);
-    add_token(&mut token_total, tkn);
-
-    vault.shares_total = shares_total.into();
-    vault.token_total = token_total.into();
+    vault.deposit(tkn);
 
     Ok(())
 }
@@ -70,19 +58,7 @@ pub fn process_withdraw(
     let mut data = vault_info.data.borrow_mut();
     let vault: &mut Vault = bytemuck::from_bytes_mut(&mut data[..]);
 
-    let mut shares_total: u64 = vault.shares_total.into();
-    let mut token_total: u64 = vault.token_total.into();
-    let tkn_for_user = if shares_total == token_total {
-        shares
-    } else {
-        mul_div_floor(shares, token_total, shares_total)
-    };
-
-    burn_shares(&mut shares_total, shares);
-    del_token(&mut token_total, tkn_for_user);
-
-    vault.shares_total = shares_total.into();
-    vault.token_total = token_total.into();
+    vault.withdraw(shares);
 
     Ok(())
 }
@@ -107,10 +83,7 @@ pub fn process_reward(
     let mut data = vault_info.data.borrow_mut();
     let vault: &mut Vault = bytemuck::from_bytes_mut(&mut data[..]);
 
-    let mut token_total: u64 = vault.token_total.into();
-    add_token(&mut token_total, tkn);
-
-    vault.token_total = token_total.into();
+    vault.reward(tkn);
 
     Ok(())
 }
@@ -135,36 +108,7 @@ pub fn process_slash(
     let mut data = vault_info.data.borrow_mut();
     let vault: &mut Vault = bytemuck::from_bytes_mut(&mut data[..]);
 
-    let mut token_total: u64 = vault.token_total.into();
-    del_token(&mut token_total, tkn);
+    vault.slash(tkn);
 
     Ok(())
-}
-
-fn add_token(token_total: &mut u64, tkn: u64) {
-    assert!(tkn > 0);
-    *token_total = token_total.checked_add(tkn).unwrap();
-}
-
-fn mint_shares(shares_total: &mut u64, shares_for_user: u64) {
-    assert!(shares_for_user > 0);
-    *shares_total = shares_total.checked_add(shares_for_user).unwrap();
-}
-
-fn burn_shares(shares_total: &mut u64, shares: u64) {
-    *shares_total = shares_total.checked_sub(shares).unwrap();
-}
-
-fn del_token(token_total: &mut u64, tkn_for_user: u64) {
-    *token_total = token_total.checked_sub(tkn_for_user).unwrap();
-}
-
-fn mul_div_floor(a: u64, b: u64, c: u64) -> u64 {
-    (a as u128)
-        .checked_mul(b as u128)
-        .unwrap()
-        .checked_div(c as u128)
-        .unwrap()
-        .try_into()
-        .unwrap()
 }
--- cvlr_by_example/vault_application/src/state.rs
+++ cvlr_by_example/vault_application/src/state.rs
@@ -9,3 +9,77 @@ pub struct Vault {
     pub shares_total: PodU64,
     pub token_total: PodU64,
 }
+
+impl Vault {
+    pub fn deposit(&mut self, tkn: u64) {
+        let mut shares_total: u64 = self.shares_total.into();
+        let mut token_total: u64 = self.token_total.into();
+        let shares_for_user = if shares_total == token_total {
+            tkn
+        } else {
+            mul_div_floor(tkn, token_total, token_total)
+        };
+
+        Self::mint_shares(&mut shares_total, shares_for_user);
+        Self::add_token(&mut token_total, tkn);
+
+        self.shares_total = shares_total.into();
+        self.token_total = token_total.into();
+    }
+
+    pub fn withdraw(&mut self, shares: u64) {
+        let mut shares_total: u64 = self.shares_total.into();
+        let mut token_total: u64 = self.token_total.into();
+        let tkn_for_user = if shares_total == token_total {
+            shares
+        } else {
+            mul_div_floor(shares, token_total, shares_total)
+        };
+
+        Self::burn_shares(&mut shares_total, shares);
+        Self::del_token(&mut token_total, tkn_for_user);
+
+        self.shares_total = shares_total.into();
+        self.token_total = token_total.into();
+    }
+
+    pub fn reward(&mut self, tkn: u64) {
+        let mut token_total: u64 = self.token_total.into();
+        Self::add_token(&mut token_total, tkn);
+        self.token_total = token_total.into();
+    }
+
+    pub fn slash(&mut self, tkn: u64) {
+        let mut token_total: u64 = self.token_total.into();
+        Self::del_token(&mut token_total, tkn);
+        self.token_total = token_total.into();
+    }
+
+    fn mint_shares(shares_total: &mut u64, shares_for_user: u64) {
+        assert!(shares_for_user > 0);
+        *shares_total = shares_total.checked_add(shares_for_user).unwrap();
+    }
+
+    fn burn_shares(shares_total: &mut u64, shares: u64) {
+        *shares_total = shares_total.checked_sub(shares).unwrap();
+    }
+
+    fn add_token(token_total: &mut u64, tkn: u64) {
+        assert!(tkn > 0);
+        *token_total = token_total.checked_add(tkn).unwrap();
+    }
+
+    fn del_token(token_total: &mut u64, tkn_for_user: u64) {
+        *token_total = token_total.checked_sub(tkn_for_user).unwrap();
+    }
+}
+
+fn mul_div_floor(a: u64, b: u64, c: u64) -> u64 {
+    (a as u128)
+        .checked_mul(b as u128)
+        .unwrap()
+        .checked_div(c as u128)
+        .unwrap()
+        .try_into()
+        .unwrap()
+}
