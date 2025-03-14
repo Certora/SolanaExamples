e8ecd3caa46845211fa4f7016da7556c31a4f607
caballa <navasjorgea@gmail.com>
2025-03-14 09:15:08 -0600

    Added missing in-bounds assumptions about the vault
    We need to ensure that all vault fields are in-bounds.
    
    The vault (from Solana account) is just uninitialized memory for the
    prover. Then, the verification state FvVault is created from the
    Solana vault but in-bounds information is not propagated, even if
    FvVault::from converts a u64 to NativeInt. Unfortunately, this not
    enough for the prover to know that FvVault fields are actually within
    u64 bounds.

--- cvlr_by_example/vault_application/src/certora/macros.rs
+++ cvlr_by_example/vault_application/src/certora/macros.rs
@@ -1,6 +1,9 @@
 #[macro_export]
 macro_rules! assume_solvency {
     ($fv_vault:expr) => {{
+        cvlr::prelude::cvlr_assume!(NativeInt::is_u64(&$fv_vault.shares_total));
+        cvlr::prelude::cvlr_assume!(NativeInt::is_u64(&$fv_vault.token_total));
+        
         cvlr::prelude::cvlr_assume!($fv_vault.shares_total <= $fv_vault.token_total);
     }};
 }
