8bf30af1e670a8ef0441284d1422043702075b13
Arie Gurfinkel <arie@certora.com>
2025-04-17 17:41:50 -0400

    chore: updated build system
    The build system used certora-sbf and our usual just configurations

--- cvlr_by_example/vault_application/certora_build.py
+++ cvlr_by_example/vault_application/certora_build.py
@@ -7,33 +7,20 @@ import sys
 import os
 from pathlib import Path
 
-SCRIPT_DIR = Path(__file__).resolve().parent
-
-# Command to run for compiling the rust project.
-COMMAND = "just build-sbf"
-
-# JSON FIELDS
-# This example uses Cargo Workspaces (https://doc.rust-lang.org/book/ch14-03-cargo-workspaces.html), and the root of the project is two levels up.
-PROJECT_DIR = (SCRIPT_DIR).resolve().parent.resolve().parent.resolve()
-# Relative from PROJECT_DIR. 
-SOURCES = [ "cvlr_by_example/vault_application/src/**/*.rs" ]
-# Relative from PROJECT_DIR. 
-EXECUTABLES = "target/sbf-solana-solana/release/vault_application.so"
 VERBOSE = False
-
+SCRIPT_DIR = Path(__file__).resolve().parent
 
 def log(msg):
     if VERBOSE:
         print(msg, file=sys.stderr)
 
-
 def run_command(command, to_stdout=False, env=None):
     """Runs the build command and dumps output to temporary files."""
-    log(f"Running '{command}'")
+    log(f"Running: {' '.join(command)}")
     try:
         if to_stdout:
             result = subprocess.run(
-                command,
+                ' '.join(command),
                 shell=True,
                 text=True,
                 cwd=SCRIPT_DIR,
@@ -42,12 +29,11 @@ def run_command(command, to_stdout=False, env=None):
             return None, None, result.returncode
         else:
             with tempfile.NamedTemporaryFile(delete=False, mode='w', prefix="certora_build_", suffix='.stdout') as stdout_file, \
-                    tempfile.NamedTemporaryFile(delete=False, mode='w', prefix="certora_build_", suffix='.stderr') as stderr_file:
+                tempfile.NamedTemporaryFile(delete=False, mode='w', prefix="certora_build_", suffix='.stderr') as stderr_file:
                 # Compile rust project and redirect stdout and stderr to a temp file
                 result = subprocess.run(
-                    command,
+                    ' '.join(command),
                     shell=True,
-                    stdout=stdout_file,
                     stderr=stderr_file,
                     text=True,
                     cwd=SCRIPT_DIR,
@@ -55,33 +41,15 @@ def run_command(command, to_stdout=False, env=None):
                 )
                 return stdout_file.name, stderr_file.name, result.returncode
     except Exception as e:
-        log(f"Error running command '{command}': {e}")
-        return None, None - 1
-
-
-def write_output(output_data, output_file=None):
-    """Writes the JSON output either to a file or dumps it to the console."""
-    if output_file:
-        with open(output_file, 'w') as f:
-            json.dump(output_data, f, indent=4)
-        log(f"Output written to {output_file}")
-    else:
-        print(json.dumps(output_data, indent=4), file=sys.stdout)
-
+        log(f"Error running command': {e}")
+        return None, None -1
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Compile rust projects and generate JSON output to be used by Certora Prover.")
-    parser.add_argument("-o", "--output", metavar="FILE",
-                        help="Path to output JSON to a file.")
-    parser.add_argument("--json", action="store_true",
-                        help="Dump JSON output to the console.")
-    parser.add_argument("-l", "--log", action="store_true",
-                        help="Show log outputs from cargo build on standard out.")
-    parser.add_argument("-v", "--verbose",
-                        action="store_true", help="Be verbose.")
-    parser.add_argument("--cargo-features", nargs="+",
-                        help="Additional features to pass to cargo")
+    parser = argparse.ArgumentParser(description="Compile rust projects and generate JSON output to be used by Certora Prover.")
+    parser.add_argument("--json", action="store_true", help="Dump JSON output to the console.")
+    parser.add_argument("--cargo_features", nargs="+", help="Additional features to pass to cargo")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Be verbose.")
+    parser.add_argument("-l", "--log", action="store_true", help="Show log outputs from cargo build on standard out.")
 
     args = parser.parse_args()
     global VERBOSE
@@ -89,39 +57,23 @@ def main():
 
     to_stdout = args.log
 
-    # pass extra features via env
-    if args.cargo_features is not None:
-        env = os.environ.copy()
-        env['CARGO_FEATURES'] = ' '.join(args.cargo_features)
-    else:
-        env = None
-
-    # Compile rust project and dump the logs to tmp files
-    stdout_log, stderr_log, return_code = run_command(COMMAND, to_stdout, env)
-
-    if stdout_log is not None:
-        log( f"Temporary log file located at:\n\t{stdout_log}\nand\n\t{stderr_log}")
-
-    # JSON template
-    output_data = {
-        "project_directory": str(PROJECT_DIR),
-        "sources": SOURCES,
-        "executables": EXECUTABLES,
-        "success": True if return_code == 0 else False,
-        "return_code": return_code,
-        "log": {"stdout": stdout_log, "stderr": stderr_log}
-    }
-
-    # Handle output based on the provided argument
-    if args.output:
-        write_output(output_data, args.output)
+    cargo_cmd = ['cargo', 'certora-sbf']
 
     if args.json:
-        write_output(output_data)
+        cargo_cmd.append("--json")
+
+    if args.cargo_features:
+        cargo_cmd.append('--features')
+        cargo_cmd.extend(args.cargo_features)
+
+    # Compile rust project and dump the logs to tmp files
+    stdout_log, stderr_log, return_code = run_command(cargo_cmd, to_stdout)
+
+    if stdout_log is not None:
+        log(f"Temporary log file located at:\n\t{stdout_log}\nand\n\t{stderr_log}")
 
     # Needed for mutations: if you run _this_ script inside another script, you can check this returncode and decide what to do
     sys.exit(0 if return_code == 0 else 1)
-
 
 if __name__ == "__main__":
     main()
